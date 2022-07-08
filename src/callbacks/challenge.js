b5afd344e3773624239bd93d491da9233309d0df
Efrel Armando López Cáceres <programmer@ferrebaratillohn.com>
2022-07-08 09:50:08 -0600

    clase 8: Fetch data

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -1,5 +1,5 @@
-const XMLHttpRequest = require("xmlhttprequest");
-const API = "https://api.escuelajs.com/api/v1";
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = " https://api.escuelajs.co/api/v1";
 
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
@@ -9,12 +9,30 @@ function fetchData(urlApi, callback) {
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error(`Error ${urlApi}`);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error(`Error ${urlApi}`);
-            return callback(error, null);
         }
     };
 
     xhttp.send();
 }
+
+fetchData(`${API}/products`, function (err, data) {
+    if (err) return console.error(err);
+
+    fetchData(`${API}/products/${data[0].id}`, function (err2, data2) {
+        if (err2) return console.error(err2);
+
+        fetchData(
+            `${API}/categories/${data2?.category?.id}`,
+            function (err3, data3) {
+                if (err3) return console.error(err3);
+                console.log(data[0]);
+                console.log(data2.title);
+                console.log(data3.name);
+            }
+        );
+    });
+});
